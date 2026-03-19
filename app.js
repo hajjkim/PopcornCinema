845eaa1fbf2711d4b4cdc5b658c51f5b5558200f
thỉ thủ mặt trăng aka Kim Ngọt <121348079+hajjkim@users.noreply.github.com>
2026-03-19 10:22:30 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+module.exports = app;
